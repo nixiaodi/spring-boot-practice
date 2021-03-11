63e51a96211eae0f8eb81014cb7493078b2d80bb
jiang <19939600096@163.com>
2021-03-11 17:32:58 +0800

    spring测试

---
+++ spring-practice/src/main/java/org/jiang/factory/TestFacoryBean.java
@@ -0,0 +1,9 @@
+package org.jiang.factory;
+    /**
+ * @Description TODO
+ * @Author jiang
+ * @Create 2021/3/11
+ * @Version 1.0
+*/
+public class TestFacoryBean {
+}
---
+++ spring-practice/src/main/java/org/jiang/model/Flower.java
@@ -0,0 +1,9 @@
+package org.jiang.model;
+    /**
+ * @Description TODO
+ * @Author jiang
+ * @Create 2021/3/11
+ * @Version 1.0
+*/
+public class Flower {
+}
